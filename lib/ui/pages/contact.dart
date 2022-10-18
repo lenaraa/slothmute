556bf5330dad3919ddd2366f9edab7ec149ec5aa
MizakiGaming <boukhebza.allan@outlook.fr>
2022-10-18 22:21:13 +0200

    feat contact list: init

--- lib/ui/pages/contact.dart
+++ lib/ui/pages/contact.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:slothmute/ui/pages/api_animaux.dart';
+
+import '../../widgets/ContactWidget.dart';
 
 class Contact extends StatefulWidget {
   const Contact({Key? key}) : super(key: key);
@@ -9,8 +10,84 @@ class Contact extends StatefulWidget {
 }
 
 class _ContactState extends State<Contact> {
+  static const List<ContactWidget> _contacts = <ContactWidget>[
+    ContactWidget(Image(image: AssetImage("assets/images/sloth4.jpg")), "Jean Guépard"),
+    ContactWidget(Image(image: AssetImage("assets/images/sloth1.jpg")), "Flash Slothmore"),
+    ContactWidget(Image(image: AssetImage("assets/images/sloth4.jpg")), "AlexSloth Sleepdt"),
+    ContactWidget(Image(image: AssetImage("assets/images/sloth3.jpg")), "Marie Paprécet"),
+    ContactWidget(Image(image: AssetImage("assets/images/sloth1.jpg")), "Jean Guépard"),
+    ContactWidget(Image(image: AssetImage("assets/images/sloth2.jpg")), "Flash Slothmore"),
+    ContactWidget(Image(image: AssetImage("assets/images/sloth4.jpg")), "AlexSloth Sleepdt"),
+    ContactWidget(Image(image: AssetImage("assets/images/sloth1.jpg")), "Marie Paprécet"),
+    ContactWidget(Image(image: AssetImage("assets/images/sloth2.jpg")), "Jean Guépard"),
+    ContactWidget(Image(image: AssetImage("assets/images/sloth3.jpg")), "Flash Slothmore"),
+    ContactWidget(Image(image: AssetImage("assets/images/sloth4.jpg")), "AlexSloth Sleepdt"),
+    ContactWidget(Image(image: AssetImage("assets/images/sloth2.jpg")), "Marie Paprécet"),
+    ContactWidget(Image(image: AssetImage("assets/images/sloth3.jpg")), "Jean Guépard"),
+    ContactWidget(Image(image: AssetImage("assets/images/sloth4.jpg")), "Flash Slothmore"),
+    ContactWidget(Image(image: AssetImage("assets/images/sloth2.jpg")), "AlexSloth Sleepdt"),
+    ContactWidget(Image(image: AssetImage("assets/images/sloth3.jpg")), "Marie Paprécet"),
+  ];
+
+
   @override
   Widget build(BuildContext context) {
-    return const ApiParesseux();
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+              flex: 1,
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(40.0),
+                  ),
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    stops: const [
+                      0.0,
+                      0.4,
+                      0.8,
+                    ],
+                    colors: <Color>[
+                      Colors.lightGreen.shade600,
+                      Colors.lightGreen.shade400,
+                      Colors.lightGreen.shade600,
+                    ],
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: const Text(
+                        "Mes amis",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+          ),
+          Expanded(
+              flex: 9,
+              child: Container(
+                margin: const EdgeInsets.only(top: 10),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(children: _contacts),
+                )
+              )
+          )
+        ],
+      )
+    ));
   }
 }
--- lib/widgets/ContactWidget.dart
+++ lib/widgets/ContactWidget.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
 
 class ContactWidget extends StatefulWidget {
-  const ContactWidget({Key? key}) : super(key: key);
+  const ContactWidget(this.photo, this.name, {super.key});
+
+  final Image photo;
+  final String name;
 
   @override
   State<ContactWidget> createState() => _ContactWidgetState();
 }
 
 class _ContactWidgetState extends State<ContactWidget> {
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.only(bottom: 5),
+      color: Colors.white,
+      child: Flex(
+        direction: Axis.horizontal,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(50.0),
+            child: Image(image: widget.photo.image, width: 50, height: 50),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: Text(widget.name),
+          ),
+          const Spacer(flex: 1),
+          Container(
+            margin: const EdgeInsets.only(right: 15),
+            child: const Icon(
+              Icons.camera_alt_rounded,
+              color: Colors.lightGreen,
+            )
+          ),
+          Container(
+            margin: const EdgeInsets.only(right: 15),
+            child: const Icon(
+              Icons.message_outlined,
+              color: Colors.lightGreen,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
